59ae80a330e667051ce96a51a9f0c6f89d4c9b0d
Nish3611 <nishupradhan96@gmail.com>
2023-07-22 22:25:06 +0545

    worked on footer and testimonials

--- index.php
+++ index.php
@@ -6,7 +6,107 @@
     <title>Comfort Store</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-    
+    <style>
+        /* Testimonial */
+.testimonial {
+    padding-top: 100px;
+  }
+  
+  .testimonial .small-test {
+    margin-top: 60px;
+  }
+  
+  .testimonial .row {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+  
+  .testimonial .col-3 {
+    text-align: center;
+    padding: 40px 20px;
+  }
+  
+  .testimonial .col-3 i {
+    color: brown;
+    font-size: 24px;
+    margin-bottom: 20px;
+  }
+  
+  .testimonial .col-3 p {
+    font-size: 16px;
+    color: black;
+    margin-bottom: 20px;
+  }
+  
+  .testimonial .col-3 .rating {
+    margin-bottom: 20px;
+  }
+  
+  .testimonial .col-3 .rating i {
+    color: brown;
+  }
+  
+  .testimonial .col-3 img {
+    width: 80px;
+    height: 80px;
+    margin-top: 20px;
+    border-radius: 50%;
+  }
+  
+  .testimonial .col-3 h3 {
+    font-size: 18px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+/* Footer styles */
+footer {
+    background-color: #333;
+    padding: 20px;
+    color: #fff;
+    text-align: center;
+  }
+  
+  .footer-content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 20px;
+  }
+  
+  .footer-content img {
+    width: 50px;
+    height: 50px;
+    margin-right: 10px;
+  }
+  
+  .footer-content p {
+    font-size: 14px;
+    margin: 0;
+  }
+  
+  .social-icons a {
+    color: #fff;
+    font-size: 20px;
+    margin: 0 10px;
+  }
+  
+  .footer-bottom p {
+    font-size: 12px;
+    margin-top: 20px;
+  }
+  
+  
+
+/* Additional styles for responsiveness */
+@media (max-width: 768px) {
+    .footer-content img {
+        width: 40px;
+        height: 40px;
+    }
+}
+    </style>
 </head>
 <body>
     
@@ -128,8 +228,55 @@
         <h3 class="title3">About Us</h3>
         <p>Welcome to Comfort Store Bag! At Comfort Store Bag, we understand the importance of combining fashion with functionality. That's why we have curated a collection of bags that effortlessly blend practicality.</p>
         <p>Comfort Store Bag was born out of a passion for offering high-quality bags that enhance your daily life. We began our journey with a simple idea: to create bags that make your everyday routines more convenient.</p>
-    </div>    
+    </div>   
+    <!-- testimonial -->
+    <div class="testimonial">
+        <div class="small-test">
+            <div class="row">
+                <div class="col-3">
+                    <i class="fa fa-quote-left"></i>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nobis adipisci nesciunt cum exercitationem reprehenderit nulla recusandae! Eius distinctio deleniti earum, iure sit reiciendis. Vel eius laboriosam dolorum nihil ex?</p>
+                    <div class="rating">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                </div>
+                <img src="images/user-1.jpg">
+                <h3>David Paul</h3>
+                </div>
+                <div class="col-3">
+                    <i class="fa fa-quote-left"></i>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nobis adipisci nesciunt cum exercitationem reprehenderit nulla recusandae! Eius distinctio deleniti earum, iure sit reiciendis. Vel eius laboriosam dolorum nihil ex?</p>
+                    <div class="rating">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                </div>
+                <img src="images/user-2.jpg">
+                <h3>Laurel Fisher</h3>
+                </div>
+                <div class="col-3">
+                    <i class="fa fa-quote-left"></i>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nobis adipisci nesciunt cum exercitationem reprehenderit nulla recusandae! Eius distinctio deleniti earum, iure sit reiciendis. Vel eius laboriosam dolorum nihil ex?</p>
+                    <div class="rating">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                </div>
+                <img src="images/user-3.jpg" width=30%>
+                <h3>Jean Baker</h3>
+                </div>
+            </div>
+        </div>
+    </div> 
 </section>
+
 <footer>
     <div class="footer-content">
         <img src="images/comfort.png" alt="Comfort Store Logo">
--- login.php
+++ login.php
@@ -1,3 +1,40 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(isset($_POST['submit'])){
+
+
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pass = md5($_POST['password']);
+    
+    $select = "SELECT *FROM user_form WHERE email = '$email' && password = '$pass' ";
+
+    $result = mysqli_query($conn, $select);
+
+    if(mysqli_num_rows($result) > 0){
+
+       $row = mysqli_fetch_array($result);
+
+       if($row['user_type'] == 'admin'){
+
+        $_SESSION['admin_name'] = $row['name'];
+        header('location:admin.php');
+
+       }elseif($row['user_type'] == 'user'){
+
+        $_SESSION['user_name'] = $row['name'];
+        header('location:user.php');
+
+    }
+    }else{
+       $error[] = 'incorrect email or password!';
+    }
+};
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -32,9 +69,15 @@
 <div class="form-container">
     <form action="" method="post">
         <h3>login now</h3>
+        <?php
+        if(isset($error)){
+            foreach($error as $error){
+                echo '<span class="error-msg">'.$error.'</span>';
+            };
+        };
+        ?>
         <input type="email" name="email" required placeholder="enter you email">
         <input type="password" name="password" required placeholder="enter you password">
-        <input type="password" name="cpassword" required placeholder="confirm your password">
         <input type="submit" name="submit" value="login now" class="form-btn">
         <p>don't have an account? <a href="register.php">register now</a></p>
     </form>
